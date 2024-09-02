f7518f93b250eb40bf519aae1e623e895f90cd37
Mortenaho <mortenaho@gmail.com>
2024-09-02 21:04:46 +0330

    init

---
+++ .idea/.idea.PersianHelper/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+G
+PersianHelper/Class1.cs,a\2\a2a20530c663514074e3d059f8a9ed467de639f1
+G
+PersianHelper/Number.cs,6\d\6d2571062f7e6b9e71999830636882cf28157ede
+Q
+!PersianHelper/PersianConverter.cs,9\8\98c9d3b90cfe9fbdbd5447577fadf745c5cf0bb1
+K
+PersianHelper/DateHelper.cs,f\b\fb277ce009ed479d74e22efe7d5e1e78104adeb3
---
+++ PersianHelper/DateHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace PersianHelper;
+
+public static class DateHelper
+{
+    public static string ToPersianDate(this DateTime miladiDate, string format = "yyyy/MM/dd")
+    {
+        // ایجاد یک شیء از کلاس PersianCalendar
+        PersianCalendar persianCalendar = new PersianCalendar();
+        
+        // تبدیل تاریخ میلادی به شمسی
+        int year = persianCalendar.GetYear(miladiDate);
+        int month = persianCalendar.GetMonth(miladiDate);
+        int day = persianCalendar.GetDayOfMonth(miladiDate);
+        
+        // جایگذاری مقادیر سال، ماه و روز در قالب مورد نظر
+        string result = format
+            .Replace("yyyy", year.ToString("D4"))
+            .Replace("yy", (year % 100).ToString("D2"))
+            .Replace("MM", month.ToString("D2"))
+            .Replace("dd", day.ToString("D2"))
+            .Replace("d", day.ToString())
+            .Replace("M", month.ToString());
+
+        return result;
+    }
+    public static DateTime ToGregorianDate(this string persianDate, string format = "yyyy/MM/dd")
+    {
+        // ایجاد یک شیء از کلاس PersianCalendar
+        PersianCalendar persianCalendar = new PersianCalendar();
+
+        // شناسایی بخش‌های تاریخ براساس فرمت
+        int year = 0, month = 0, day = 0;
+
+        // جایگذاری مقادیر سال، ماه و روز براساس فرمت
+        if (format.Contains("yyyy"))
+        {
+            year = int.Parse(persianDate.Substring(format.IndexOf("yyyy"), 4));
+        }
+        else if (format.Contains("yy"))
+        {
+            year = int.Parse(persianDate.Substring(format.IndexOf("yy"), 2)) + 1300;
+        }
+
+        if (format.Contains("MM"))
+        {
+            month = int.Parse(persianDate.Substring(format.IndexOf("MM"), 2));
+        }
+        else if (format.Contains("M"))
+        {
+            month = int.Parse(persianDate.Substring(format.IndexOf("M"), 1));
+        }
+
+        if (format.Contains("dd"))
+        {
+            day = int.Parse(persianDate.Substring(format.IndexOf("dd"), 2));
+        }
+        else if (format.Contains("d"))
+        {
+            day = int.Parse(persianDate.Substring(format.IndexOf("d"), 1));
+        }
+
+        // ساخت تاریخ میلادی
+        return persianCalendar.ToDateTime(year, month, day, 0, 0, 0, 0);
+    }
+}
